ca3351700976f67cd91b52fe7ac977fd6130ea9a
IDA Kenichiro <ida.kenichiro@gmail.com>
2025-09-10 23:40:56 +0900

    Fix Biome comments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
-import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
 import { App } from "./App.tsx";
 
+// biome-ignore lint/style/noNonNullAssertion: 必ず #root が存在する前提なので null チェックはしない
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
